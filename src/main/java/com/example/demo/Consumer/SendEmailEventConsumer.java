f3227ff2400e906cc5c80bc3db3fb926daec37ce
Charan <charansaianisetti3@gmial.com>
2024-11-29 11:37:56 +0530

    This is a email consumer servier for the Kafka Implementation

---
+++ src/main/java/com/example/demo/Consumer/SendEmailEventConsumer.java
@@ -0,0 +1,57 @@
+package com.example.demo.Consumer;
+
+
+
+
+import com.example.demo.Dtos.SendEmailEventDto;
+import com.example.demo.Utils.EmailUtil;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.common.protocol.types.Field;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import java.util.Properties;
+
+@Service
+public class SendEmailEventConsumer {
+    private ObjectMapper objectMapper;
+
+    public SendEmailEventConsumer(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @KafkaListener(topics = "sendEmail", groupId = "emailService")
+    public void handleSendEmailEvent(String message) throws JsonProcessingException {
+        SendEmailEventDto emailEventDto = objectMapper.readValue(
+                message, SendEmailEventDto.class
+        );
+
+        String to = emailEventDto.getTo();
+        String from = emailEventDto.getFrom();
+        String subject = emailEventDto.getSubject();
+        String body = emailEventDto.getBody();
+
+        //Send an Email.
+        System.out.println("Email Start");
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com"); //SMTP Host
+        props.put("mail.smtp.port", "587"); //TLS Port
+        props.put("mail.smtp.auth", "true"); //enable authentication
+        props.put("mail.smtp.starttls.enable", "true"); //enable STARTTLS
+
+        //create Authenticator object to pass in Session.getInstance argument
+        Authenticator auth = new Authenticator() {
+            //override the getPasswordAuthentication method
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from, "iqfhzuuzbnergzrk");
+            }
+        };
+        Session session = Session.getInstance(props, auth);
+
+        EmailUtil.sendEmail(session, to, subject, body);
+    }
+}
